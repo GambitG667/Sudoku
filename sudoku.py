4a18e0c165e0242eebc6e8a32850f8eef3f620c4
ArtymP <117117917+GambitG667@users.noreply.github.com>
2023-01-02 16:34:41 +0300

    Add files via upload

---
+++ sudoku.py
@@ -0,0 +1,91 @@
+import pygame as pg
+from wawe_colaps import create_sudoku, Field
+
+WIDTH = 540  # ширина игрового окна
+HEIGHT = 540 # высота игрового окна
+FPS = 30 # частота кадров в секунду
+
+WID = 40 #Ширина сетки
+HEI = 40 #Высота сетки
+
+BLACK = (47, 79, 79)
+WHITE = (255, 228, 225)
+RED = (205, 92, 92)
+GREEN = (50, 205, 50)
+BLUE = (30, 144, 255)
+
+pg.init()
+pg.font.init()
+pg.mixer.init()
+sc = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("Sudoku")
+clock = pg.time.Clock()
+
+font = pg.font.SysFont('couriernew', 20)
+big_font = pg.font.SysFont('couriernew', 60)
+
+text = font.render("YLLOU", True, BLACK)
+sudoku = create_sudoku()
+
+def draw_sels():
+    for i in range(1,9):
+        if i%3 == 0:
+            pg.draw.line(sc, WHITE, (i*60,0),(i*60,540),3)
+            pg.draw.line(sc, WHITE, (0,i*60),(540,i*60),3)
+        else:
+            pg.draw.line(sc, WHITE, (i*60,0),(i*60,540))
+            pg.draw.line(sc, WHITE, (0,i*60),(540,i*60))
+
+def render():
+    global sc
+    sc.fill(BLACK)
+    draw_sels()
+    for i in range(9):
+        for j in range(9):
+            if sudoku[i][j].value != 0:
+                text = big_font.render(f"{sudoku[i][j].value}", True, WHITE)
+                sc.blit(text, (i*60+10, j*60))
+            else:
+                for k in range(3):
+                    for n in range(3):
+                        if sudoku[i][j].superposition[n*3+k]:
+                            text = font.render(f"{n*3+k+1}", True, WHITE)
+                        else:
+                            text = font.render(f"{n*3+k+1}", True, BLUE)
+                        sc.blit(text,(i*60+k*20+5,j*60+n*20))
+
+def render2():
+    sc.fill(BLACK)
+    for i in range(27):
+        for j in range(27):
+            if sudoku[i][j]:
+                text = font.render("1", True, WHITE)
+            else:
+                text = font.render("0", True, WHITE)
+            sc.blit(text,(i*20,j*20))
+
+
+running = True
+ply = False
+while running:
+
+    clock.tick(FPS)
+    render()
+
+    for i in pg.event.get():
+        # check for closing window
+        if i.type == pg.QUIT:
+            running = False
+        elif i.type == pg.MOUSEBUTTONDOWN:
+            if i.button == 1:
+                x,y = i.pos[0]//60, i.pos[1]//60
+                i = i.pos[0]//20%3 + i.pos[1]//20%3*3
+                sudoku[x][y].UpdateSuperposition(i+1)
+        elif i.type == pg.KEYDOWN:
+            if i.key == pg.K_r:
+                sudoku = create_sudoku()
+
+    
+    pg.display.flip()
+
+pg.quit()
---
+++ wawe_colaps.py
@@ -0,0 +1,42 @@
+import random
+
+class Field():
+    
+    def __init__(self):
+        self.superposition = []
+        self.neighbors = []
+        self.value = 0
+        for i in range(9):
+            self.superposition.append(True)
+
+    def set_neighbor(self, neighbors):
+        self.neighbors = neighbors
+    
+    def UpdateSuperposition(self, pos_val):
+        if self.value == 0 and self.superposition[pos_val-1]:
+            self.value = pos_val
+            for i in self.neighbors:
+                i.close_value(pos_val)
+    
+    def close_value(self, value):
+        if (not self.value):
+            self.superposition[value-1] = False
+
+
+def create_sudoku():
+    sudoku = []
+    for i in range(9):
+        sudoku.append([])
+        for j in range(9):
+            sudoku[i].append(Field())
+    for i in range(9):
+        for j in range(9):
+            naibors = []
+            for k in range(9):
+                naibors.append(sudoku[k][j])
+                naibors.append(sudoku[i][k])
+            for k in range(3):
+                for m in range(3):
+                    naibors.append(sudoku[i//3*3+k][j//3*3+m])
+            sudoku[i][j].set_neighbor(naibors)
+    return sudoku
